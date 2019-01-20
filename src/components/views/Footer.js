fae3ec56f50b9986c295dfae31b9799becb3e1d2
Jasper De gooijer <jasperdegooijer@jaspers-mbp.localdomain>
2019-01-20 17:21:05 +0100

    changed EF text a bit

--- src/components/views/Footer.js
+++ src/components/views/Footer.js
@@ -7,7 +7,7 @@ class Footer extends Component {
 	render() {
 		return (
 			<div id="footer">
-				<p>Powered by: The Ethereum foundation</p>
+				<p>Made possible by: The Ethereum foundation</p>
 				<img className="footer-img" alt="Ethereum foundation logo" src={ethLogo}/>
 			</div>
 		)
